1bd69f4c732854531ec913fa01d8eae73628738d
gh0stface1z <julliajutotu120121@gmail.com>
2026-08-24 00:57:26 -0400

    Implement click event to open scrapbook
    Add click event listener to open scrapbook with delay.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const caderno = document.getElementById("caderno");
 
+let abrindo = false;
+
+caderno.addEventListener("click", () => {
+
+  if (abrindo) return;
+
+  abrindo = true;
+
+  caderno.classList.add("abrindo");
+
+  setTimeout(() => {
+
+    /*
+      Depois vamos trocar isso para:
+      window.location.href = "scrapbook.html";
+    */
+
+    console.log("Abrir scrapbook");
+
+  }, 800);
+
+});
